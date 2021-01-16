3a71bbf5ada87d09cb86d04becf620c931841010
salzg <salzguerkchen@googlemail.com>
2021-01-16 14:54:34 +0100

    initial

---
+++ Uebung8/main.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+		shoptitel: 'Lager Buchshop Gxx',
+		products: [],
+		timestamp : ""
+    },
+    methods: {
+		fetchData(){
+			fetch("dbjson.json")
+			.then(response => response.json())
+			.then((data) => {
+			  this.products = data.products;
+			})
+		},
+		increase(index){
+			this.products[index].Lagerbestand++;
+		},
+		decrease(index){
+			if(this.products[index].Lagerbestand > 0)
+				this.products[index].Lagerbestand--;
+		},
+		now(){
+			const today = new Date();
+			const date = today.getDate()
+							+ '.'+(today.getMonth()+1)
+							+ '.'+today.getFullYear();
+			const time = today.getHours() 
+							+ ":" + today.getMinutes()
+							+ ":" + today.getSeconds();
+			const dateTime = "Datum: " + date +' Uhrzeit: '+ time;
+			this.timestamp  = dateTime;
+		}
+	},
+	created () {
+			setInterval(this.now, 1000);
+	},
+	mounted () {
+			this.fetchData();
+	},
+});
